122c5fe57997c1e48c35dbcceacc57f2f3053c61
Radek Tomiška <radek.tomiska@gmail.com>
2019-09-13 12:45:07 +0200

    prepare to use new test for codeable componente

---
+++ Realization/backend/idm-archetype/src/main/resources/archetype-resources/Realization/backend/idm-__mid__/src/test/java/__packageInPathFormat__/__mid__/config/__mn__ConfigurableIntegrationTest.java
@@ -0,0 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+package ${groupId}.${mid}.config;
+
+import ${czechIdMGroupId}.test.api.AbstractConfigurableIntegrationTest;
+
+/**
+ * Check module provided configurations.
+ *
+ * @author ${author}
+ *
+ */
+public class ${mn}ConfigurableIntegrationTest extends AbstractConfigurableIntegrationTest {
+
+}
